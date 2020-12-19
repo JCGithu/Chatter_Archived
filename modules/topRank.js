1e2974ec4aebe939f034862e41e869e294acb40e
jcgithu <jack.c.gracie@gmail.com>
2020-12-19 18:07:32 +0000

    prettier reformat

--- modules/topRank.js
+++ modules/topRank.js
@@ -1,21 +1,25 @@
 const { knex } = require('./KnexOpts.js');
 
 topRank = (top, socket, callback) => {
-    return new Promise((resolve, reject) => {
-        knex.from('users').select('*').orderBy('points', 'desc').then((table) => {
-            let lineArray = [];
-            top = top[top.length - 1]
-            for (let j = 0; j < top; j++) {
-                let line = [{position: j+1, name: table[j].user_name, points: table[j].points}];
-                lineArray.push(line);
-                if (j == top - 1){
-                    resolve(lineArray);
-                }
-            }
-        });
-    });
-}
+  return new Promise((resolve, reject) => {
+    knex
+      .from('users')
+      .select('*')
+      .orderBy('points', 'desc')
+      .then((table) => {
+        let lineArray = [];
+        top = top[top.length - 1];
+        for (let j = 0; j < top; j++) {
+          let line = [{ position: j + 1, name: table[j].user_name, points: table[j].points }];
+          lineArray.push(line);
+          if (j == top - 1) {
+            resolve(lineArray);
+          }
+        }
+      });
+  });
+};
 
 module.exports = {
-    topRank
+  topRank,
 };
